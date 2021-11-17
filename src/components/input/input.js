6f139d961b24ff78088b6217bc2bfa5396f7419a
modrayyes <mod.rayyes@gmail.com>
2021-11-17 14:30:36 +0200

    .

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,12 +1,45 @@
 import React from 'react'
+import { connect } from 'react-redux';
+import { addTodo, addText, editAddTodo } from '../../redux/action';
 
-function Input() {
+function Input({ addText, text, selected, addTodo }) {
+
+const handleChange = e => addText(e.target.value);
+const handleSubmit = e => {
+    e.preventDefault();
+    if (selected || selected === 0)
+      editAddTodo({
+        value: text,
+        selected: selected
+      });
+    else addTodo(text);
+  };
     return (
-        <form>
-            <input type='text' name='todo' placeholder='Enter todo'/>
-            
-        </form>
+        <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="todo"
+          value={text}
+          placeholder="Enter your todo..."
+          onChange={handleChange}
+        />
+      </form>
     )
 }
 
-export default Input
+
+const mapDispatchToProps = dispatch => ({
+    addTodo: todo => dispatch(addTodo(todo)),
+    addText: value => dispatch(addText(value)),
+    editAddTodo: obj => dispatch(editAddTodo(obj))
+  });
+  const mapStateToProps = ({ text, selected }) => ({
+    text,
+    selected
+  });
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Input);
+  
